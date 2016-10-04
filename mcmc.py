424243bdc792821eefa1cca990fce8ad396629ff
Dillon Brout <djbrout@gmail.com>
2016-10-04 13:37:51 -0400

    getting ready to try mcmc

--- mcmc.py
+++ mcmc.py
@@ -277,7 +277,7 @@ class metropolis_hastings():
         for i in np.arange(Nimage):
             for x in np.arange(substamp):
                 for y in np.arange(substamp):
-                    if np.sqrt((substamp/2. - x)**2 + (substamp/2. - y)**2) < self.fitrad:
+                    if np.sqrt(((substamp-1)/2. - x + 1)**2 + ((substamp-1)/2. - y + 1)**2) < self.fitrad:
                         #self.skyerr[i,int(x),int(y)] = skyerr[i]
                         tempgalmodel[int(x),int(y)] = copy(self.galaxy_model[int(x),int(y)])
                         self.mask[int(x),int(y)] = 1.
