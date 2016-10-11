6cc036cdfb469d7695f4ec7af064cc7f6387a9f7
Dillon Brout <djbrout@gmail.com>
2016-10-11 13:06:22 -0400

    shifting psf with buildpsfex

--- SEaR.py
+++ SEaR.py
@@ -176,7 +176,7 @@ class model:
 
 
 
-        self.data[0, :, :] = self.data[1, :, :] + self.psfs[0,:,:]*10000.
+        self.data[0, :, :] = self.data[1, :, :] + self.psfs[0,:,:]*20000.
 
     def runDMC(self):
         ts = time.time()
