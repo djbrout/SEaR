165ab8c517224b274ae8a16e56b304b41447bf17
Dillon Brout <djbrout@gmail.com>
2016-10-22 19:44:04 -0400

    getting ready to run on edison

--- SEaR.py
+++ SEaR.py
@@ -108,7 +108,7 @@ class fit:
         tmphdr = pf.getheader(self.template)
 
         self.imzpt = imhdr['HIERARCH DOFAKE_ZP']
-        print tmphdr.keys()
+        print tmphdr
         self.templatezpt = tmphdr['SEXMGZPT']
         self.imagesky = imhdr['SKYBRITE']
         self.imageskyerr = imhdr['SKYSIGMA']
