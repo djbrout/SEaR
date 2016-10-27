6401543ffc5007dd281707bbebcc000a8f117c3d
Dillon Brout <djbrout@gmail.com>
2016-10-27 08:34:30 -0400

    getting ready to run on edison

--- SEaR.py
+++ SEaR.py
@@ -267,10 +267,10 @@ class fit:
         self.weights[1,:,:] = templateweightdata[self.templatepsfcenter[1] - self.stampsize/2:self.templatepsfcenter[1] + self.stampsize/2,
                            self.templatepsfcenter[0] - self.stampsize/2:self.templatepsfcenter[0] + self.stampsize/2]
 
-        # print max([self.templatepsfcenter[1]-50.,0]),min([self.templatepsfcenter[1]+50, templatedata.shape[1]-1]),max([self.templatepsfcenter[0] - 50., 0]),min([self.templatepsfcenter[0] + 50, templatedata.shape[0] - 1])
+        print max([self.templatepsfcenter[1]-50.,0]),min([self.templatepsfcenter[1]+50, templatedata.shape[1]-1]),max([self.templatepsfcenter[0] - 50., 0]),min([self.templatepsfcenter[0] + 50, templatedata.shape[0] - 1])
         print 'templatemean',np.mean(templatedata[max([self.templatepsfcenter[1]-50.,0]):min([self.templatepsfcenter[1]+50, templatedata.shape[1]-1]),
                                               max([self.templatepsfcenter[0] - 50., 0]):min([self.templatepsfcenter[0] + 50, templatedata.shape[0] - 1])])
-
+        raw_input()
         # mean, st, vals = sigma_clip.meanclip(templatedata[max([self.templatepsfcenter[1]-50.,0]):min([self.templatepsfcenter[1]+50, templatedata.shape[1]-1]),
         #                                      max([self.templatepsfcenter[0] - 50., 0]):min([self.templatepsfcenter[0] + 50, templatedata.shape[0] - 1])],
         #                                      clipsig=4, maxiter=8)
