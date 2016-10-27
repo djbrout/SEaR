130211161fe838ad8096cb5c7f1db696a950074e
Dillon Brout <djbrout@gmail.com>
2016-10-27 08:42:56 -0400

    getting ready to run on edison

--- SEaR.py
+++ SEaR.py
@@ -302,9 +302,9 @@ class fit:
     def runDMC(self):
         ts = time.time()
         aaa = mcmc.metropolis_hastings(
-              galmodel=     self.data[1,:,:]#setting the initial guess of the galaxy/background model to the template image
+              galmodel=     self.data[1,:,:]/10.#setting the initial guess of the galaxy/background model to the template image
             , modelvec=     np.array([self.initialguess,0])
-            , galstd=       np.sqrt(np.abs(self.data[1,:,:]))/2. + 20.
+            , galstd=       np.sqrt(np.abs(self.data[1,:,:]))/20. + 20.
             , modelstd=     np.array([self.stepstd,0.])
             , data=         self.data
             , psfs=         self.psfs
