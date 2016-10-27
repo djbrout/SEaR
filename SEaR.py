fcf6b08c7690db8534fe8ad9f96edbb67630018a
Dillon Brout <djbrout@gmail.com>
2016-10-27 08:37:17 -0400

    getting ready to run on edison

--- SEaR.py
+++ SEaR.py
@@ -268,9 +268,10 @@ class fit:
                            self.templatepsfcenter[0] - self.stampsize/2:self.templatepsfcenter[0] + self.stampsize/2]
 
         print self.templatepsfcenter
+        print templatedata.shape
         print max([self.templatepsfcenter[1]-50.,0]),min([self.templatepsfcenter[1]+50, templatedata.shape[1]-1]),max([self.templatepsfcenter[0] - 50., 0]),min([self.templatepsfcenter[0] + 50, templatedata.shape[0] - 1])
         print 'templatemean',np.mean(templatedata[max([self.templatepsfcenter[1]-50.,0]):min([self.templatepsfcenter[1]+50, templatedata.shape[1]-1]),
-                                              max([self.templatepsfcenter[0] - 50., 0]):min([self.templatepsfcenter[0] + 50, templatedata.shape[0] - 1])])
+                                              max([self.templatepsfcenter[0] - 50., 0]):min([self.templatepsfcenter[0] + 50, templatedata.shape[1] - 1])])
         raw_input()
         # mean, st, vals = sigma_clip.meanclip(templatedata[max([self.templatepsfcenter[1]-50.,0]):min([self.templatepsfcenter[1]+50, templatedata.shape[1]-1]),
         #                                      max([self.templatepsfcenter[0] - 50., 0]):min([self.templatepsfcenter[0] + 50, templatedata.shape[0] - 1])],
