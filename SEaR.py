705700cdfee724d7e7ec7756e0d7e38b8686c401
Dillon Brout <djbrout@gmail.com>
2016-10-26 23:13:58 -0400

    getting ready to run on edison

--- SEaR.py
+++ SEaR.py
@@ -267,7 +267,7 @@ class fit:
         self.weights[1,:,:] = templateweightdata[self.templatepsfcenter[1] - self.stampsize/2:self.templatepsfcenter[1] + self.stampsize/2,
                            self.templatepsfcenter[0] - self.stampsize/2:self.templatepsfcenter[0] + self.stampsize/2]
 
-        mean, st, vals = sigma_clip.meanclip(imagedata[max([self.templatepsfcenter[1]-50.,0]):min([self.templatepsfcenter[1]+50, templatedata.shape[1]-1]),
+        mean, st, vals = sigma_clip.meanclip(templatedata[max([self.templatepsfcenter[1]-50.,0]):min([self.templatepsfcenter[1]+50, templatedata.shape[1]-1]),
                                              max([self.templatepsfcenter[0] - 50., 0]):min([self.templatepsfcenter[0] + 50, templatedata.shape[0] - 1])],
                                              clipsig=4, maxiter=8)
         #self.templateskyerr = 1.48 * np.median(abs(vals - np.median(vals)))
