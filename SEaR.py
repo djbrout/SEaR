161709af70ea0a0f372dbdd5211c92f8309eb78f
Dillon Brout <djbrout@gmail.com>
2016-11-05 18:57:52 -0400

    WORKS NOW! getting ready to run wrapper

--- SEaR.py
+++ SEaR.py
@@ -447,8 +447,7 @@ class fit:
         numdefaults = 0
 
         ix, iy, tx, ty, imagepath, templatepath, imagepsf, templatepsf, imageweight, templateweight, imagezpt, templatezpt, imagesky, templatesky, imageskyerr, templateskyerr = None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None
-        print self.rootdir
-        raw_input()
+
         for o, a in opt:
             if o in ["-h", "--help"]:
                 print __doc__
@@ -459,6 +458,8 @@ class fit:
             elif o in ["-r", "--rootdir"]:
                 if self.rootdir is None:
                     self.rootdir = a
+                    print self.rootdir
+                    raw_input('rootdir')
             elif o in ["--floatpos"]:
                 if self.floatpos is None:
                     self.floatpos = True
