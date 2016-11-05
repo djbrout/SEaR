ee3a12758b2f085b0c0f58401f7a8c11413af29b
Dillon Brout <djbrout@gmail.com>
2016-11-05 19:09:29 -0400

    WORKS NOW! getting ready to run wrapper

--- wrapper.py
+++ wrapper.py
@@ -11,12 +11,13 @@ sd = 'seardetections'
 searout = open(sd, 'w')
 searout.write('band_ccd, x, y, sn, mag, schi, tchi\n')
 searout.close()
-for bc,x,y,sn,m in zip(detections['band_ccd'],detections['x'],detections['y'],detections['sn'],detections['mag']):
+for i,bc,x,y,sn,m in zip(range(len(detections['x'])),detections['band_ccd'],detections['x'],detections['y'],
+                         detections['sn'],detections['mag']):
     band = bc.split('_')[0]
     ccd = bc.split('_')[1]
     if not band in bandlist: continue
     if not ccd in ccdlist: continue
-    chisqs = SEaR.fit(ix=x,iy=y)
+    chisqs = SEaR.fit(ix=x,iy=y,candid='test_'+str(i))
     print chisqs
     searout = open(sd,'a')
     searout.write(bc+','+str(x)+','+str(y)+','+str(sn)+','+str(m)+','+str(round(chisqs[0],3))+','+str(round(chisqs[1],3))+'\n')
