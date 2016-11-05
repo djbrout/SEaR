cb9d2b9e9bb1efa5c02e6f424743192fc0fbc206
Dillon Brout <djbrout@gmail.com>
2016-11-05 19:50:43 -0400

    Switchin nominal zpt to the image (searhc) zpt

--- wrapper.py
+++ wrapper.py
@@ -11,8 +11,11 @@ sd = 'seardetections.txt'
 searout = open(sd, 'w')
 searout.write('band_ccd, x, y, sn, mag, searmag, schi, tchi\n')
 searout.close()
+cntr = 0
 for i,bc,x,y,sn,m in zip(range(len(detections['x'])),detections['band_ccd'],detections['x'],detections['y'],
                          detections['sn'],detections['mag']):
+    cntr += 1
+    if cntr > 10: continue
     band = bc.split('_')[0]
     ccd = bc.split('_')[1]
     if not band in bandlist: continue
