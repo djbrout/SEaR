838f0affda8159e9aad45017d35a5cdf08184c33
Dillon Brout <djbrout@gmail.com>
2016-11-04 22:53:00 -0400

    getting ready to run on edison

--- SEaR.py
+++ SEaR.py
@@ -317,7 +317,7 @@ class fit:
             #raw_input()
         if not self.templateskyerr is None:
             self.weights[1, :, :] = np.ones(self.weights[1,:,:].shape)/ self.templateskyerr ** 2
-        print self.templatezpt, self.imgzpt
+        print self.templatezpt, self.imzpt
         raw_input('template zpt')
         #if not self.templatezpt is None:
         #    self.data[1, :, :] *= 10 ** (.4*(31. - self.templatezpt))
