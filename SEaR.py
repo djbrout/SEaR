14674cbde0607bca191c77d27833a1aea745af92
Dillon Brout <djbrout@gmail.com>
2016-11-05 19:05:08 -0400

    WORKS NOW! getting ready to run wrapper

--- SEaR.py
+++ SEaR.py
@@ -203,7 +203,8 @@ class fit:
 
         docntrd = True
         if docntrd:
-            self.ix, self.iy = cntrd.cntrd(imagedata, [self.ix], [self.iy], 5.)
+            print self.ix, self.iy
+            self.ix, self.iy = cntrd.cntrd(imagedata, [float(self.ix)], [float(self.iy)], 5.)
             print 'just centroided',self.ix,self.iy
             raw_input()
             ihl = fits.open(self.image)
