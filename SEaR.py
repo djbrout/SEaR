909ff58471501a23343ca5c95b142139c0a5082e
Dillon Brout <djbrout@gmail.com>
2016-11-05 22:06:11 -0400

    Switchin nominal zpt to the image (searhc) zpt

--- SEaR.py
+++ SEaR.py
@@ -355,8 +355,8 @@ class fit:
             self.templatesky *= 10 ** (.4*(self.imzpt - self.templatezpt))
             self.templateskyerr *= 10 ** (.4*(self.imzpt - self.templatezpt))
 
-        self.data[1, :, :] = self.data[1, :, :].T
-        self.weights[1, :, :] = self.weights[1, :, :].T
+        self.data[0, :, :] = self.data[0, :, :].T
+        self.weights[0, :, :] = self.weights[0, :, :].T
         print 'skyyyyy',self.templatesky,self.imagesky
         print 'skyyyyyerrrrr',self.templateskyerr,self.imageskyerr
         #self.data[0, :, :] = self.data[1, :, :] + self.psfs[0,:,:]*20000.
