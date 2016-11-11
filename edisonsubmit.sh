f5f6ea369816ce07678c24260f6725eae188bef3
Dillon Brout <djbrout@gmail.com>
2016-11-11 12:19:34 -0500

    added edison job submission

--- edisonsubmit.sh
+++ edisonsubmit.sh
@@ -1,8 +1,14 @@
 #!/bin/csh
-
+echo 1
 cd  /global/u1/d/dbrout/SEaR/
+echo 2
 module load python
+echo 3
 bash
+echo 4
 source /global/project/projectdirs/dessn/diffim/setup.sh
+echo 5
 diffimg
+echo 6
 python wrapper.py --ccd=$1
+echo 7
