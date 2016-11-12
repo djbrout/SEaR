39f6e0b6d29d25d9d8b765186815ee5422be3188
Dillon Brout <djbrout@gmail.com>
2016-11-11 19:11:51 -0500

    doing my own fft

--- mcmc.py
+++ mcmc.py
@@ -646,15 +646,16 @@ class metropolis_hastings():
                     # gc = np.roll(gc, -m / 2 + 1, axis=0)
                     # gc = np.roll(gc, -n / 2 + 1, axis=1)
 
-                    delta = np.fft.fftn(S * fr2).real
+                    delta = np.fft.fftn(kicked_modelvec * S * fr2).real
                     #delta = np.fft.fftshift(FS).real
 
                     #gc = ifft(fft(centered_psfs)).real
-                    sims = (delta + sky) * self.mask
+                    galaxy_conv = scipy.signal.fftconvolve(self.kicked_galaxy_model, centered_psfs, mode='same')
+                    sims = (galaxy_conv + delta + sky) * self.mask
 
                     #print 'simshape',sims.shape
                     #THIS IS THE OLD WAY
-                    # galaxy_conv = scipy.signal.fftconvolve(self.kicked_galaxy_model, centered_psfs,mode='same')
+                    #
                     # star_conv = kicked_modelvec * kicked_psfs/np.sum(kicked_psfs.ravel())
                     # sims =  (star_conv + galaxy_conv + sky)*self.mask
 
