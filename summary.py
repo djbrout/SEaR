05d71532fb88e62591f5265c39c24602409b78a7
Dillon Brout <djbrout@gmail.com>
2016-11-12 13:22:34 -0500

    added summary script

--- summary.py
+++ summary.py
@@ -8,3 +8,20 @@ os.system('cat /scratch1/scratchdirs/dbrout/p9/results3/detections_i_* > detecti
 data = dt.readcol('detections_i_all.txt',delim=',')
 
 print data.keys()
+
+import matplotlib as m
+m.use('Agg')
+import matplotlib.pyplot as plt
+
+sn = data['sn']
+chsq1 = data['search_1fwhm_chisq']
+chsq2 = data['search_2fwhm_chisq']
+diffmag = data['mag']
+
+wreal = diffmag > 0
+wfake = diffmag == 0
+
+plt.scatter(sn[wreal],chsq2[wreal],color='green',size=20)
+plt.scatter(sn[wfake],chsq2[wfake],color='red',size=20)
+plt.savefig('/scratch1/scratchdirs/dbrout/p9/results3/results.png')
+print 'saved /scratch1/scratchdirs/dbrout/p9/results3/results.png'
