ba453e3ddb9494eb981e391bcf76473c0b779808
Dillon Brout <djbrout@gmail.com>
2016-11-11 19:06:00 -0500

    doing my own fft

--- mcmc.py
+++ mcmc.py
@@ -633,7 +633,7 @@ class metropolis_hastings():
                     #gc = ifft(fft(self.kicked_galaxy_model)*fft(centered_psfs)*
                     #          np.exp(1j*(freq*10.0+self.x_pix_offset+freq*10.0+self.y_pix_offset))).real
 
-                    [X, Y] = np.meshgrid(np.arange(20)/10000.,np.arange(20)/10000.)
+                    [X, Y] = np.meshgrid(np.arange(20)/10.,np.arange(20)/10.)
 
                     S = np.exp(1j*(X*(1.+self.x_pix_offset)+Y*(1.+self.y_pix_offset)))
 
