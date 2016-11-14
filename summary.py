992fb5a77494ef150567191758ab17bbe6612f5f
Dillon Brout <djbrout@gmail.com>
2016-11-14 10:11:59 -0500

    back to se errors

--- summary.py
+++ summary.py
@@ -24,9 +24,9 @@ diffmag = data['mag']
 wreal = (diffmag > 0)
 wfake = (diffmag == 0)
 
-plt.scatter(sn[wfake],chsq2[wfake]-chsq1[wfake],color='red',alpha=.5)
-plt.scatter(sn[wreal],chsq2[wreal]-chsq1[wreal],color='green',alpha=.9)
-plt.xlim(4.,20.)
-plt.ylim(0,5.)
+plt.scatter(chisq1[wfake],chsq2[wfake]-chsq1[wfake],color='red',alpha=.5)
+plt.scatter(chisq1[wfake],chsq2[wreal]-chsq1[wreal],color='green',alpha=.9)
+plt.xlim(0.,10.)
+plt.ylim(-.1,.1)
 plt.savefig('/scratch1/scratchdirs/dbrout/p9/results4/results.png')
 print 'saved /scratch1/scratchdirs/dbrout/p9/results4/results.png'
