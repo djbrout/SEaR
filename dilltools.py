8d8f57e3a208001140ba6cd184e37bca529c5853
Dillon Brout <djbrout@gmail.com>
2016-11-14 09:55:27 -0500

    back to se errors

--- dilltools.py
+++ dilltools.py
@@ -141,7 +141,7 @@ def readcol(filename,headline=1,startline=2,delim=' '):
     for line in inf:
         line = line.replace('#', '')
         if linenum >= startline - 1:
-            line= line[0:60]+','+line[60:]
+            line= line[0:61]+','+line[61:]
         #raw_input()
         line = line.strip()
         cols = line.split(delim)
