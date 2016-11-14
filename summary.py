84fc865130308805a05f1be117719a65f259366b
Dillon Brout <djbrout@gmail.com>
2016-11-14 08:42:41 -0500

    back to se errors

--- summary.py
+++ summary.py
@@ -3,7 +3,7 @@ import dilltools as dt
 import os
 
 
-os.system('cat /scratch1/scratchdirs/dbrout/p9/results3/detections_i_* > detections_i_all.txt')
+os.system('cat /scratch1/scratchdirs/dbrout/p9/results4/detections_i_* > detections_i_all.txt')
 
 data = dt.readcol('detections_i_all.txt',delim=',')
 
@@ -25,5 +25,5 @@ plt.scatter(sn[wreal],chsq2[wreal],color='green',alpha=.5)
 plt.scatter(sn[wfake],chsq2[wfake],color='red',alpha=.5)
 plt.xlim(5.,20.)
 plt.ylim(0,5.)
-plt.savefig('/scratch1/scratchdirs/dbrout/p9/results3/results.png')
-print 'saved /scratch1/scratchdirs/dbrout/p9/results3/results.png'
+plt.savefig('/scratch1/scratchdirs/dbrout/p9/results4/results.png')
+print 'saved /scratch1/scratchdirs/dbrout/p9/results4/results.png'
