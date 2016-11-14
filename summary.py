f33b76e78a8fa1f115277b2c9bde1b17eae9ecbb
Dillon Brout <djbrout@gmail.com>
2016-11-14 10:28:10 -0500

    back to se errors

--- summary.py
+++ summary.py
@@ -35,7 +35,7 @@ print 'saved /scratch1/scratchdirs/dbrout/p9/results4/results.png'
 
 plt.clf()
 plt.hist([chsq2[wfake]-chsq1[wfake],chsq2[wreal]-chsq1[wreal]],color=['red','green'],bins=np.arange(-3.5,2,.2),normed=True)
-plt.xlim(-3.5.,2.)
+plt.xlim(-3.5,2.)
 plt.xlabel('Chisq 2FWHM - 1FWHM')
 plt.savefig('/scratch1/scratchdirs/dbrout/p9/results4/resultshist.png')
 print 'saved /scratch1/scratchdirs/dbrout/p9/results4/resultshist.png'
