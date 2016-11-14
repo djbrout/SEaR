5140c9690521c1879047a1c3b989b5f74abd4aa5
Dillon Brout <djbrout@gmail.com>
2016-11-14 11:20:03 -0500

    back to se errors

--- summary.py
+++ summary.py
@@ -57,8 +57,8 @@ for i in np.arange(.2,2.,.02):
 
             p = float(len(diffmag[wwbad]))/float((len(diffmag[wwreal])+len(diffmag[wwbad])))
             e = float(len(diffmag[wwreal]))/float(nreal)
-            if p > .9:
-                if e > .6:
+            if p > .92:
+                if e > .67:
                     print 'upperlimchi',upperlimchi,'lowerlimchi',lowerlimchi,'upperlimdiff',upperlimdiff,'Purity',p,'Eff',e
 
 
