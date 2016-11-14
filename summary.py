4169a7fb9a3eb9fb16b03aef3cf3749124f0cffb
Dillon Brout <djbrout@gmail.com>
2016-11-14 10:20:18 -0500

    back to se errors

--- summary.py
+++ summary.py
@@ -24,15 +24,18 @@ diffmag = data['mag']
 wreal = (diffmag > 0) & (chsq1 < 1000) & (chsq1 >= 0.)
 wfake = (diffmag == 0) & (chsq1 < 1000) & (chsq1 >= 0.)
 
-plt.scatter(chsq1[wfake],chsq2[wfake]-chsq1[wfake],color='red',alpha=.5)
-plt.scatter(chsq1[wreal],chsq2[wreal]-chsq1[wreal],color='green',alpha=.9)
-plt.xlim(0.,10.)
-plt.ylim(-.1,.1)
+plt.scatter(sn[wfake],chsq2[wfake],color='red',alpha=.5)
+plt.scatter(sn[wreal],chsq2[wreal],color='green',alpha=.9)
+plt.xlim(0.,20.)
+plt.ylim(0,20)
+plt.ylabel('2 FWHM Chi Squared')
+plt.xlabel('S/N')
 plt.savefig('/scratch1/scratchdirs/dbrout/p9/results4/results.png')
 print 'saved /scratch1/scratchdirs/dbrout/p9/results4/results.png'
 
 plt.clf()
 plt.hist([chsq2[wfake]-chsq1[wfake],chsq2[wreal]-chsq1[wreal]],color=['red','green'],bins=np.arange(-.5,.5,.1),normed=True)
 plt.xlim(-.5,.5)
+plt.xlabel('Chisq 2FWHM - 1FWHM')
 plt.savefig('/scratch1/scratchdirs/dbrout/p9/results4/resultshist.png')
 print 'saved /scratch1/scratchdirs/dbrout/p9/results4/resultshist.png'
