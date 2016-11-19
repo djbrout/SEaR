902b2590be6ef6a6a817674befc8ebab1655c6f5
Dillon Brout <djbrout@gmail.com>
2016-11-19 09:34:55 -0500

    custom errors

--- summary.py
+++ summary.py
@@ -15,17 +15,17 @@ import matplotlib.pyplot as plt
 
 train = 600
 sn = data['sn'][:train]
-print max(sn)
+#print max(sn)
 chsq1 = data['search_1fwhm_chisq'][:train]
 chsq2 = data['search_2fwhm_chisq'][:train]
 chsq3 = data['search_3fwhm_chisq'][:train]
 tcs = data['templ_chi'][:train]
 diffmag = data['mag'][:train]
 fitmag = data['sm_mag'][:train]
-print sn[diffmag==20]
-print tcs.shape
-print chsq1.shape
-raw_input()
+#print sn[diffmag==20]
+#print tcs.shape
+#print chsq1.shape
+#raw_input()
 
 snlim = 4.
 
@@ -34,19 +34,21 @@ snlim = 4.
 
 nreal = len(diffmag[(diffmag>0) & (diffmag != 20.001) & (sn > snlim)])
 nbad = len(diffmag[(diffmag==0)& (sn > snlim)])
+ntot = len(diffmag[(sn > snlim)])
 
-
-wreal = (diffmag > 0) & (chsq1 < 1000) & (chsq1 >= 0.) & (diffmag != 20.001)#& (sn > snlim)
-wfake = (diffmag == 0) & (chsq1 < 1000) & (chsq1 >= 0.)#& (sn > snlim)
+wreal = (diffmag > 0) & (chsq3 < 1000) & (chsq3 >= 0.) & (diffmag != 20.001)#& (sn > snlim)
+wfake = (diffmag == 0) & (chsq3 < 1000) & (chsq3 >= 0.)#& (sn > snlim)
 
 ll = .79
 ul = 1.16
+s=.01
 
 plt.scatter(sn[wfake],chsq2[wfake],color='red',alpha=.5)
 plt.scatter(sn[wreal],chsq2[wreal],color='green',alpha=.9)
 plt.axhline(ll,color='black',linestyle='--')
-plt.axhline(ul,color='black',linestyle='--')
-plt.xlim(4.,80.)
+plt.plot([0,10,500],[ul,ul,500*s + ul],color='black',linestyle='--')
+#plt.axhline(ul,color='black',linestyle='--')
+plt.xlim(4.,150.)
 plt.ylim(0,20.)
 plt.ylabel('2 FWHM Chi Squared')
 plt.xlabel('S/N')
@@ -57,8 +59,9 @@ plt.clf()
 plt.scatter(sn[wfake],chsq1[wfake],color='red',alpha=.5)
 plt.scatter(sn[wreal],chsq1[wreal],color='green',alpha=.9)
 plt.axhline(ll,color='black',linestyle='--')
-plt.axhline(ul,color='black',linestyle='--')
-plt.xlim(4.,80.)
+plt.plot([0,10,500],[ul,ul,500*s + ul],color='black',linestyle='--')
+#plt.axhline(ul,color='black',linestyle='--')
+plt.xlim(4.,150.)
 plt.ylim(0,20.)
 plt.ylabel('2 FWHM Chi Squared')
 plt.xlabel('S/N')
@@ -69,8 +72,9 @@ plt.clf()
 plt.scatter(sn[wfake],chsq3[wfake],color='red',alpha=.5)
 plt.scatter(sn[wreal],chsq3[wreal],color='green',alpha=.9)
 plt.axhline(ll,color='black',linestyle='--')
-plt.axhline(ul,color='black',linestyle='--')
-plt.xlim(4.,80.)
+plt.plot([0,10,500],[ul,ul,500*s + ul],color='black',linestyle='--')
+#plt.axhline(ul,color='black',linestyle='--')
+plt.xlim(4.,150.)
 plt.ylim(0,20.)
 plt.ylabel('2 FWHM Chi Squared')
 plt.xlabel('S/N')
@@ -109,31 +113,40 @@ maxpe = 0
 ulc = 0
 llc = 0
 uld = 0
+ps = 0
 maxp = 0
 maxe = 0
-for i in np.arange(0.3,.99,.01):
-    for j in np.arange(.1,2.5,.01):
-        for k in np.arange(-4.,0,.1):
-            upperlimchi = i+j
-            lowerlimchi = i
-            upperlimdiff = k
-            wwreal = (chsq2 > lowerlimchi) & (chsq2 < upperlimchi) & (diffmag > 0) & (diffmag != 20.001) & (sn > snlim)
-            wwreal2 = (chsq2-chsq1 < upperlimdiff) & (diffmag > 0) & (diffmag != 20.001) & (sn > snlim)
-            wwbad = (chsq2 > lowerlimchi) & (chsq2 < upperlimchi) & (diffmag == 0) & (sn > snlim)
-            wwbad2 = (chsq2-chsq1 < upperlimdiff) & (diffmag == 0) & (sn > snlim)
-            #print diffmag[np.logical_or(wwbad, wwbad2)]
-            #print diffmag[np.logical_or(np.logical_or(np.logical_or(wwreal,wwreal2),wwbad,wwbad2))]
-            p = 1 - float(len(diffmag[np.logical_or(wwbad, wwbad2)]))/float(len(diffmag[np.logical_or(np.logical_or(np.logical_or(wwreal,wwreal2),wwbad),wwbad2)]))
-            e = float(len(diffmag[np.logical_or(wwreal, wwreal2)]))/float(nreal)
-            if p+e > maxpe:
-                ulc = upperlimchi
-                llc = lowerlimchi
-                uld = upperlimdiff
-                maxp = p
-                maxe = e
-                maxpe = p+e
-            #if p+e > 1.909:
-            print 'upperlimchi',upperlimchi,'lowerlimchi',lowerlimchi,'upperlimdiff',upperlimdiff,'Purity',round(p,3),'Eff',round(e,3)
+snsplit = 10.
+for i in np.arange(0.6,.9,.01):
+    for j in np.arange(.3,1.3,.01):
+        for k in np.arange(-2.,-.5,.1):
+            for s in np.arange(0,.1,.01):
+                upperlimchi = i+j
+                lowerlimchi = i
+                upperlimdiff = k
+                wwreal = (chsq3 > lowerlimchi) & (chsq3 < upperlimchi) & (diffmag > 0) & (diffmag != 20.001) & (sn > snlim) & (sn < snsplit)
+                wwreal2 = (chsq3 > lowerlimchi) & (chsq3 < (s*sn)+upperlimchi) & (diffmag > 0) & (diffmag != 20.001) & (sn > snlim) & (sn > snsplit)
+                wwreal3 = (chsq3-chsq1 < upperlimdiff) & (diffmag > 0) & (diffmag != 20.001) & (sn > snlim)
+
+                wwbad = (chsq3 > lowerlimchi) & (chsq3 < upperlimchi) & (diffmag == 0) & (sn > snlim)  & (sn < snsplit)
+                wwbad2 = (chsq3 > lowerlimchi) & (chsq3 < (s*sn)+upperlimchi) & (diffmag == 0) & (sn > snlim) & (sn > snsplit)
+                wwbad3 = (chsq3-chsq1 < upperlimdiff) & (diffmag == 0) & (sn > snlim)
+
+                p = 1 - float(len(diffmag[np.logical_or(wwbad3, np.logical_or(wwbad,
+                        wwbad2))]))/float(len(diffmag[np.logical_or(wwbad3,
+                        np.logical_or(wwreal3,np.logical_or(np.logical_or(np.logical_or(wwreal,
+                        wwreal2),wwbad),wwbad2)))]))
+                e = float(len(diffmag[np.logical_or(wwreal3,np.logical_or(wwreal, wwreal2))]))/float(nreal)
+                if p+e > maxpe:
+                    ulc = upperlimchi
+                    llc = lowerlimchi
+                    uld = upperlimdiff
+                    ps = s
+                    maxp = p
+                    maxe = e
+                    maxpe = p+e
+                #if p+e > 1.909:
+                print 'upperlimchi',upperlimchi,'lowerlimchi',lowerlimchi,'upperlimdiff',upperlimdiff,'Purity',round(p,3),'Eff',round(e,3)
 
 print '-'*50
 print '-'*50
@@ -145,38 +158,39 @@ print '-'*50
 upperlimchi = ulc
 lowerlimchi = llc
 upperlimdiff = uld
+s = ps
 
-sn = data['sn']#[1000:]
-chsq1 = data['search_1fwhm_chisq']#[1000:]
-chsq2 = data['search_2fwhm_chisq']#[1000:]
-tcs = data['templ_chi']#[1000:]
-diffmag = data['mag']#[1000:]
+sn = data['sn'][train:]
+chsq1 = data['search_1fwhm_chisq'][train:]
+chsq2 = data['search_2fwhm_chisq'][train:]
+tcs = data['templ_chi'][train:]
+diffmag = data['mag'][train:]
 
-wwreal = (chsq2 > lowerlimchi) & (chsq2 < upperlimchi) & (diffmag > 0) & (diffmag != 20.001) & (sn > snlim)
-wwreal2 = (chsq2-chsq1 < upperlimdiff) & (diffmag > 0) & (diffmag != 20.001) & (sn > snlim)
+wwreal = (chsq3 > lowerlimchi) & (chsq3 < upperlimchi) & (diffmag > 0) & (diffmag != 20.001) & (sn > snlim) & (sn < snsplit)
+wwreal2 = (chsq3 > lowerlimchi) & (chsq3 < (s*sn)+upperlimchi) & (diffmag > 0) & (diffmag != 20.001) & (sn > snlim) & (sn > snsplit)
+wwreal3 = (chsq3-chsq1 < upperlimdiff) & (diffmag > 0) & (diffmag != 20.001) & (sn > snlim)
 
-wwbad = (chsq2 > lowerlimchi) & (chsq2 < upperlimchi) & (diffmag == 0) & (sn > snlim)
-wwbad2 = (chsq2-chsq1 < upperlimdiff) & (diffmag == 0) & (sn > snlim)
+wwbad = (chsq3 > lowerlimchi) & (chsq3 < upperlimchi) & (diffmag == 0) & (sn > snlim)  & (sn < snsplit)
+wwbad2 = (chsq3 > lowerlimchi) & (chsq3 < (s*sn)+upperlimchi) & (diffmag == 0) & (sn > snlim) & (sn > snsplit)
+wwbad3 = (chsq3-chsq1 < upperlimdiff) & (diffmag == 0) & (sn > snlim)
 
-nreal = len(diffmag[(diffmag>0) & (diffmag != 20.001) & (sn > snlim)])
-nbad = len(diffmag[(diffmag==0)& (sn > snlim)])
-
-
-p = 1 - float(len(diffmag[np.logical_or(wwbad,wwbad2)]))/float(len(diffmag[np.logical_or(np.logical_or(np.logical_or(wwreal,wwreal2),wwbad),wwbad2)]))
-e = float(len(diffmag[np.logical_or(wwreal,wwreal2)]))/float(nreal)
-
+p = 1 - float(len(diffmag[np.logical_or(wwbad3, np.logical_or(wwbad,
+                        wwbad2))]))/float(len(diffmag[np.logical_or(wwbad3,
+                        np.logical_or(wwreal3,np.logical_or(np.logical_or(np.logical_or(wwreal,
+                        wwreal2),wwbad),wwbad2)))]))
+e = float(len(diffmag[np.logical_or(wwreal3,np.logical_or(wwreal, wwreal2))]))/float(nreal)
 print '*'*50
 print '*'*50
 print 'upperlimchi',upperlimchi,'lowerlimchi',lowerlimchi,'upperlimdiff',upperlimdiff,'Purity',round(p,3),'Eff',round(e,3)
 print ''
-print 'total',len(diffmag[np.logical_or(np.logical_or(np.logical_or(wwreal,wwreal2),wwbad),wwbad2)])
-print 'contamination',len(diffmag[np.logical_or(wwbad,wwbad2)])
+print 'total',ntot
+print 'contamination',len(diffmag[np.logical_or(wwbad3, np.logical_or(wwbad,wwbad2))])
 print ''
 print 'total good',nreal
-print 'fit good',len(diffmag[np.logical_or(wwreal, wwreal2)])
+print 'fit good',len(diffmag[np.logical_or(wwreal3,np.logical_or(wwreal, wwreal2))])
 
 print ''
 print 'total bad',nbad
-print 'eliminated',len(diffmag[np.logical_or(wwbad, wwbad2)])
+print 'eliminated',len(diffmag[np.logical_or(wwbad3,np.logical_or(wwbad, wwbad2))])
 print '*'*50
 print '*'*50
