3402cd6ecc491d531c6e01a735e69fc28ba6b2ca
Dillon Brout <djbrout@gmail.com>
2016-11-19 15:29:34 -0500

    custom errors

--- summary.py
+++ summary.py
@@ -158,14 +158,14 @@ for i in np.arange(0.77,.85,.01):
                         np.logical_or(wwreal3,np.logical_or(np.logical_or(np.logical_or(wwreal,
                         wwreal2),wwbad),wwbad2)))]))
                 e = float(len(diffmag[np.logical_or(wwreal3,np.logical_or(wwreal, wwreal2))]))/float(nreal)
-                if p+e > maxpe:
+                if p+1.2*e > maxpe:
                     ulc = upperlimchi
                     llc = lowerlimchi
                     uld = upperlimdiff
                     ps = copy(s)
                     maxp = p
                     maxe = e
-                    maxpe = p+e
+                    maxpe = p+1.2*e
                 #if p+e > 1.909:
                 print 'upperlimchi',upperlimchi,'lowerlimchi',lowerlimchi,'upperlimdiff',upperlimdiff,'slope',s,'Purity',round(p,3),'Eff',round(e,3)
                 #raw_input()
