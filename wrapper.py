95da1cba3d0f99162cb8f7c5af9cfb87f4da468b
Dillon Brout <djbrout@gmail.com>
2016-11-29 12:39:51 -0500

    sim back in error

--- wrapper.py
+++ wrapper.py
@@ -32,7 +32,7 @@ def run(tccd):
         if not ccd == tccd: continue
         cntr += 1
         print cntr
-        if cntr < 31: continue
+        if cntr < 27: continue
         classifier = SEaR.fit(ix=x,iy=y,candid='test_'+str(i)+'_'+band+'_'+ccd,ccd=ccd)
         chisqs, fitmag, fitmagerr, cx, cy, chisq1fwhm, chisq2fwhm, chisq3fwhm = classifier.go()
         print chisqs
