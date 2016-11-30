fb35d7fa191f1973aae299c1a78467100befb028
Dillon Brout <djbrout@gmail.com>
2016-11-29 22:49:12 -0500

    sim back in error

--- loop.py
+++ loop.py
@@ -20,7 +20,10 @@ for i in np.arange(0, 10):
         '#SBATCH --gres=craynetwork:1\n' +
         '\n' +
         'cd  /global/u1/d/dbrout/SEaR/\n' +
-        'source /global/u1/d/dbrout/SEaR/edisonsubmit.sh ' + str(i) + ' \n' +
+        'source /global/project/projectdirs/dessn/diffim/setup.sh\n'+
+        'diffimg\n'
+        'python wrapper.py --ccdi='+str(i)+' \n'
+        #'source /global/u1/d/dbrout/SEaR/edisonsubmit.sh ' + str(i) + ' \n' +
         '\n'
     )
     f.close()
