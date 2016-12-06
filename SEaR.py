acdf4099789ca6fa8d3537765921cf1142ab409f
Dillon Brout <djbrout@gmail.com>
2016-12-06 14:41:01 -0500

    index run

--- SEaR.py
+++ SEaR.py
@@ -433,7 +433,7 @@ class fit:
         py.savefig('ps.png')
 
         py.clf()
-        py.imshow(image,interpolation='nearest', vmin=min(image), vmax=max(image),cmap='gray')
+        py.imshow(image,interpolation='nearest', vmin=min(image.ravel()), vmax=max(image.ravel()),cmap='gray')
         py.savefig('psi.png')
 
         import runsextractor
