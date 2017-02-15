3e1bacb8a2e2b68c0cbdd1d8e92b6a72e568ad06
Dillon Brout <djbrout@gmail.com>
2017-02-15 08:07:14 -0500

    mpp added for parallel processing

--- loop.py
+++ loop.py
@@ -4,22 +4,22 @@ import numpy as np
 
 nproc=4
 
-for i in np.arange(0, 10):
+for i in np.arange(0, 4000):
     print i
     script = '/global/u1/d/dbrout/SEaR/submission_scripts/sm_' + str(i) + '.sh'
     f = open(script, 'w')
     f.write(
         '#!/bin/bash -l\n' +
         '#SBATCH --partition=shared\n' +
-        '#SBATCH -n 1\n' +
+        '#SBATCH -n 12\n' +
         '#SBATCH -A des\n' +
-        '#SBATCH --time=00:10:00\n' +
+        '#SBATCH --time=00:50:00\n' +
         '#SBATCH --output=/scratch1/scratchdirs/dbrout/searscratch/sm_' + str(i) + '_v21_1.log\n' +
         '#SBATCH --error=/scratch1/scratchdirs/dbrout/searscratch/sm_' + str(i) + '_v21_1.log\n' +
         '#SBATCH --job-name=2_iband_' + str(i) + '\n' +
         '#SBATCH --mail-type=All\n' +
         '#SBATCH --qos=premium\n'+
-        '#SBATCH --mail-user=djbrout@gmail.com\n' +
+        #'#SBATCH --mail-user=djbrout@gmail.com\n' +
         '#SBATCH --gres=craynetwork:1\n' +
         '\n' +
         'cd /global/u1/d/dbrout/SEaR/\n' +
