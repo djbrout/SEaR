e5afcdf64208abbf9f56ac147b7651e9a0a85742
Dillon Brout <djbrout@gmail.com>
2017-02-15 16:13:43 -0500

    mpp added for parallel processing

--- loop.py
+++ loop.py
@@ -13,9 +13,9 @@ for i in np.arange(0, 4000):
         '#SBATCH --partition=shared\n' +
         '#SBATCH -n 12\n' +
         '#SBATCH -A des\n' +
-        '#SBATCH --time=00:26:00\n' +
-        '#SBATCH --output=/scratch1/scratchdirs/dbrout/searscratch/sm_' + str(i) + '_v21_1.log\n' +
-        '#SBATCH --error=/scratch1/scratchdirs/dbrout/searscratch/sm_' + str(i) + '_v21_1.log\n' +
+        '#SBATCH --time=00:30:00\n' +
+        '#SBATCH --output=/scratch1/scratchdirs/dbrout/searscratch/sm_' + str(i) + '_v22_1.log\n' +
+        '#SBATCH --error=/scratch1/scratchdirs/dbrout/searscratch/sm_' + str(i) + '_v22_1.log\n' +
         '#SBATCH --job-name=2_iband_' + str(i) + '\n' +
         #'#SBATCH --mail-type=NONE\n' +
         '#SBATCH --qos=premium\n'+
