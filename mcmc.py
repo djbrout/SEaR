acd381e4c5b7204961d266068ab2f5468ff8dfe9
Dillon Brout <djbrout@gmail.com>
2017-02-18 14:34:34 -0500

    mpp added for parallel processing

--- mcmc.py
+++ mcmc.py
@@ -455,7 +455,7 @@ class metropolis_hastings():
                         self.alreadyextended = True
                         stop = True
                     if chsqs[0] > 0:
-                        self.maxiter = self.maxiter + 40000
+                        self.maxiter = self.maxiter + 60000
                         self.alreadyextended = True
                         stop = False
                     # elif chsqs[0] > 1.:
