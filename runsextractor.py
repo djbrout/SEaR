41ba0db720996d0ff52b22f6dd928b225977e0a3
Dillon Brout <djbrout@gmail.com>
2017-02-19 21:32:52 -0500

    mpp added for parallel processing

--- runsextractor.py
+++ runsextractor.py
@@ -66,8 +66,8 @@ def getsky_and_skyerr(imagefilename,imagedata,xlow,xhi,ylow,yhi,index=''):
     bg = pf.getdata('sewpy_logs/'+index+'.background')
     bgrms = pf.getdata('sewpy_logs/'+index+'.background_rms')
 
-    os.system('yes | rm  sewpy_logs/' + index + '.background')
-    os.system('yes | rm  sewpy_logs/' + index + '.background_rms')
+    os.remove('sewpy_logs/' + index + '.background')
+    os.system('sewpy_logs/' + index + '.background_rms')
     os.system('yes | rm sewpy_logs/'+index+'*.txt')
     os.popen('rm sewpy_logs/'+index+'*')
 
