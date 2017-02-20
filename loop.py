cfcad970bd8fa0071300c7d997ae6431d305b423
Dillon Brout <djbrout@gmail.com>
2017-02-20 12:12:15 -0500

    mpp added for parallel processing

--- loop.py
+++ loop.py
@@ -19,7 +19,7 @@ for i in allindexes:
         '#SBATCH --time=00:19:00\n' +
         '#SBATCH --output=/scratch1/scratchdirs/dbrout/searscratch/sm_' + str(i) + '_v22_0.log\n' +
         '#SBATCH --error=/scratch1/scratchdirs/dbrout/searscratch/sm_' + str(i) + '_v22_0.log\n' +
-        '#SBATCH --job-name=3_iband_' + str(i) + '\n' +
+        '#SBATCH --job-name=2_iband_' + str(i) + '\n' +
         '#SBATCH --mail-type=NONE\n' +
         '#SBATCH --qos=premium\n'+
         '#SBATCH --mail-user=bdrizzle@yahoo.com\n' +
