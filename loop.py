5fe85f1ae20359fcdb617b43fcf485b6896e4a8c
Dillon Brout <djbrout@gmail.com>
2017-02-23 17:51:45 -0500

    mpp added for parallel processing

--- loop.py
+++ loop.py
@@ -35,7 +35,7 @@ for i in allindexes:
         'echo "--start='+str(i*nproc)+' --stop='+str((i+1)*nproc)+'" \n'+
         #'python mpp.py --start='+str(i*nproc)+' --stop='+str((i+1)*nproc)+' \n'
         #'python mpp.py --start=' + str(i * nproc) + ' --stop=' + str((i + 1) * nproc) + ' \n'
-        'source /global/u1/d/dbrout/SEaR/edisonsubmit.sh ' + str(i) + ' 2 \n' +
+        'source /global/u1/d/dbrout/SEaR/edisonsubmit.sh ' + str(i) + ' 6 \n' +
         '\n'
     )
     f.close()
