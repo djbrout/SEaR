8f34fd0e5b05b6408e050e79d8d4fef7ff6f8f63
Dillon Brout <djbrout@gmail.com>
2017-02-23 20:53:18 -0500

    mpp added for parallel processing

--- loop.py
+++ loop.py
@@ -4,7 +4,7 @@ import numpy as np
 import time
 nproc=4
 
-allindexes = range(0000,4000)
+allindexes = range(0,500)
 np.random.shuffle(allindexes)
 
 for i in allindexes:
@@ -35,7 +35,7 @@ for i in allindexes:
         'echo "--start='+str(i*nproc)+' --stop='+str((i+1)*nproc)+'" \n'+
         #'python mpp.py --start='+str(i*nproc)+' --stop='+str((i+1)*nproc)+' \n'
         #'python mpp.py --start=' + str(i * nproc) + ' --stop=' + str((i + 1) * nproc) + ' \n'
-        'source /global/u1/d/dbrout/SEaR/edisonsubmit.sh ' + str(i) + ' 5 \n' +
+        'source /global/u1/d/dbrout/SEaR/edisonsubmit.sh ' + str(i) + ' 6 \n' +
         '\n'
     )
     f.close()
