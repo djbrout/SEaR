b3a9abad027bf9c3e860c8720cc5e9e36e0b466c
Dillon Brout <djbrout@gmail.com>
2017-03-25 22:14:44 -0400

    mpp added for parallel processing

--- SEaR.py
+++ SEaR.py
@@ -424,7 +424,9 @@ class fit:
         else:
             xhi = np.floor(self.tx) + (self.stampsize - 1) / 2 + 1
 
-        self.data[1,:,:] = templatedata[int(self.impsfcenter[1] - self.stampsize/2):int(self.impsfcenter[1] + self.stampsize/2),int(self.impsfcenter[0] - self.stampsize/2):int(self.impsfcenter[0] + self.stampsize/2)]
+        print self.data[1,:,:].shape
+        self.data[1,:,:] = templatedata[int(self.impsfcenter[1] - self.stampsize/2):int(self.impsfcenter[1] + self.stampsize/2),
+                           int(self.impsfcenter[0] - self.stampsize/2):int(self.impsfcenter[0] + self.stampsize/2)]
 
         self.weights[1,:,:] = np.swapaxes(templateweightdata[int(self.impsfcenter[1] - self.stampsize/2):int(self.impsfcenter[1] + self.stampsize/2),
                            int(self.impsfcenter[0] - self.stampsize/2):int(self.impsfcenter[0] + self.stampsize/2)],0,1)
